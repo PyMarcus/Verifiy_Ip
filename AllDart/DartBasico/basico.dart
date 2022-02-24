e9c3844af6075d79b3b9e25b20454ac5947d5562
PyMarcus <marcus-v@outlook.com>
2022-02-24 09:08:51 -0300

    commit inicial

---
+++ AllDart/DartBasico/basico.dart
@@ -0,0 +1,21 @@
+void main(){
+
+  /*declaração de variaveis*/
+  var variavel = 3; // tipo definido conforme o conteudo
+  const variavel1 = 5; // não é possível alterar o tipo da variavel ja declarada,sem parse
+  String variavel2 = "olá";
+  int varivel3 = 3;
+  double variavel4 = 3.4;
+  dynamic variavel5 = 'a'; //permite alterar o tipo posteriormente
+  bool variavel6 = true;
+  print(variavel);
+  print("variáveis $variavel6");
+  print("Variavel ${variavel}");
+  int total = 3 + 4;
+  int sub = 3 - 4;
+  int div = 4 / 2;
+  int mod = 4 % 2;
+  int mult = 4 * 2;
+  
+  print(total);
+}
